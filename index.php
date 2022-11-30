a6e0cac273c94043490207816c29a2cb126216f9
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-30 16:43:27 +0100

    esercizio finito

--- index.php
+++ index.php
@@ -13,16 +13,28 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS only -->
+  <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous">
   <title>PHP Badwords</title>
 </head>
 <body>
 
-  <?php
 
-
-?>
-
-
+  
+  <div class="container">
+    <div class="row">
+    <h1 class="mb-5">PHP Badwords</h1>
+    <form action="./pagina-di-atterraggio.php" method="POST" class="d-flex flex-column">
+      <input type="text" name="insulto" placeholder="scrivi la parola da censurare" >
+      <textarea type="text" name="paragrafo" placeholder="scrivi un insulto con la parola da censurare"></textarea>
+      <button type="submit">insulta</button>
+    </form>
+</div>
+</div>
 
 </body>
 </html>
--- pagina-di-atterraggio.php
+++ pagina-di-atterraggio.php
@@ -1,6 +1,6 @@
 <?php
-
-
+$insulto = $_POST['insulto'];
+$paragrafo =$_POST['paragrafo']; 
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,35 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous">
   <title>Document</title>
 </head>
 <body>
-  
+  <div class="container">
+    <div class="row">
+
+    
+      <h1>L'insulto che hai scritto è: <?php echo $insulto ?></h1>
+
+      <p><?php echo $paragrafo?></p>
+      <h2>lunghezza paragrafo <?php echo strlen($paragrafo)?></h2>
+
+
+      <h2>censuriamo l'insulto</h2>
+      <p><?php echo str_replace($insulto, '***', $paragrafo)?></p>
+
+
+      <?php echo $paragrafo_esploso = explode(' ', $paragrafo ) ?>
+ 
+
+      <?php var_dump( $paragrafo_esploso) ?>
+
+
+    </div>
+  </div>
 </body>
 </html>
